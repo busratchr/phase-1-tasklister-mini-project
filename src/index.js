d45ad89240e277fcbd266c548b2f9c1d6b2ce939
BUSRA KUCUK <busra3590@gmail.com>
2023-04-25 15:34:14 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  
+  form.addEventListener('submit', (event) =>{
+
+    event.preventDefault();
+  }
+  )
+
 });
